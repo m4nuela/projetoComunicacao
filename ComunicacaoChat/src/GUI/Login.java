d9623ef9d3ab7d87a38f7331757343b40c2d0b43
m4nuela <manu_ela10@hotmail.com>
2014-02-25 16:16:09 -0300

    anyway 2

--- ComunicacaoChat/src/GUI/Login.java
+++ ComunicacaoChat/src/GUI/Login.java
@@ -47,6 +47,7 @@ public class Login extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
